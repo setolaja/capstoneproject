db335f81da4679df8f2929989399fd48cc1a93e0
James Setola <jss420@lehigh.edu>
2019-02-18 21:43:07 -0500

    Added voice to text code to App.java and added future dependencies to pom.xml

--- projectmain/src/main/java/capstoneproject/App.java
+++ projectmain/src/main/java/capstoneproject/App.java
@@ -1,13 +1,64 @@
 package capstoneproject;
 
-/**
- * Hello world!
- *
- */
+// Imports the Google Cloud client library
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    /**
+    * Demonstrates using the Speech API to transcribe an audio file.
+    */
+    private void testSpeechToText() {
+        // Instantiates a client
+        try {
+            SpeechClient speechClient = SpeechClient.create();
+            // The path to the audio file to transcribe
+            String fileName = "./resources/audio.raw";
+    
+            // Reads the audio file into memory
+            Path path = Paths.get(fileName);
+            byte[] data = Files.readAllBytes(path);
+            ByteString audioBytes = ByteString.copyFrom(data);
+    
+            // Builds the sync recognize request
+            RecognitionConfig config = RecognitionConfig.newBuilder()
+                .setEncoding(AudioEncoding.LINEAR16)
+                .setSampleRateHertz(16000)
+                .setLanguageCode("en-US")
+                .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder()
+                .setContent(audioBytes)
+                .build();
+    
+            // Performs speech recognition on the audio file
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+    
+            for (SpeechRecognitionResult result : results) {
+            // There can be several alternative transcripts for a given chunk of speech. Just use the
+            // first (most likely) one here.
+            SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+            System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            }
+        }
+        catch (Exception e) {
+
+        }
     }
 }
