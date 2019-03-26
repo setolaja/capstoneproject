b4e3d1f9e322eb172d0bcd04ed9d976018705b2d
James Setola <jss420@lehigh.edu>
2019-03-25 23:05:20 -0400

    Implemented CMU Sphinx4 for speech recognition. Set up a baseline grammar model to test speech to text for our 10 queries. Reconfigured some files.

--- projectmain/src/main/java/capstoneproject/App.java
+++ projectmain/src/main/java/capstoneproject/App.java
@@ -1,94 +1,31 @@
 package capstoneproject;
 
-// Imports the Google Cloud client library
-import com.google.cloud.speech.v1.RecognitionAudio;
-import com.google.cloud.speech.v1.RecognitionConfig;
-import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
-import com.google.cloud.speech.v1.RecognizeResponse;
-import com.google.cloud.speech.v1.SpeechClient;
-import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
-import com.google.cloud.speech.v1.SpeechRecognitionResult;
-import com.google.protobuf.ByteString;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        testDBConnection();
+
+        // get the Postgres configuration from the environment
+        // Map<String, String> env = System.getenv();
+        // String ip = env.get("POSTGRES_IP");
+        // String port = env.get("POSTGRES_PORT");
+        // String user = env.get("POSTGRES_USER");
+        // String pass = env.get("POSTGRES_PASS");
+
+        // Get a fully-configured connection to the database, or exit 
+        // immediately
+        // Database db = Database.getDatabase(ip, port, user, pass);
+        // if (db == null)
+        //     return;
+        System.out.println("Working Dir: " + System.getProperty("user.dir"));
+        new SpeechRecognizerMain();
     }
 
-    /**
-    * Demonstrates using the Speech API to transcribe an audio file.
-    */
-    private static void testSpeechToText() {
-        // Instantiates a client
-        try {
-            SpeechClient speechClient = SpeechClient.create();
-            // The path to the audio file to transcribe
-            String fileName = "./resources/audio.raw";
-    
-            // Reads the audio file into memory
-            Path path = Paths.get(fileName);
-            byte[] data = Files.readAllBytes(path);
-            ByteString audioBytes = ByteString.copyFrom(data);
-    
-            // Builds the sync recognize request
-            RecognitionConfig config = RecognitionConfig.newBuilder()
-                .setEncoding(AudioEncoding.LINEAR16)
-                .setSampleRateHertz(16000)
-                .setLanguageCode("en-US")
-                .build();
-            RecognitionAudio audio = RecognitionAudio.newBuilder()
-                .setContent(audioBytes)
-                .build();
-    
-            // Performs speech recognition on the audio file
-            RecognizeResponse response = speechClient.recognize(config, audio);
-            List<SpeechRecognitionResult> results = response.getResultsList();
-    
-            for (SpeechRecognitionResult result : results) {
-            // There can be several alternative transcripts for a given chunk of speech. Just use the
-            // first (most likely) one here.
-            SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-            System.out.printf("Transcription: %s%n", alternative.getTranscript());
-            }
-        }
-        catch (Exception e) {
-
-        }
-    }
-
-    private static void testDBConnection() {
-        String db_url = env.get("DATABASE_URL");
-        // Give the Database object a connection, fail if we cannot get one
-        try {
-            Class.forName("org.postgresql.Driver");
-            URI dbUri = new URI(db_url);
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "sslmode=require";
-            Connection conn = DriverManager.getConnection(dbUrl, username, password);
-            if (conn == null) {
-                System.err.println("Error: DriverManager.getConnection() returned a null object");
-                return null;
-            }
-            db.mConnection = conn;
-        } catch (SQLException e) {
-            System.err.println("Error: DriverManager.getConnection() threw a SQLException");
-            e.printStackTrace();
-            return null;
-        } catch (ClassNotFoundException cnfe) {
-            System.out.println("Unable to find postgresql driver");
-            return null;
-        } catch (URISyntaxException s) {
-            System.out.println("URI Syntax Error");
-            return null;
-        }
-        System.out.println("Connection Successful");
-    }
 }
